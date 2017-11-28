af98790c30d3af0e13f8026aa039a13c301ec8ea
Nowfel Mashnoor <nmmashnoor@gmail.com>
2017-11-28 16:06:54 +0600

    fixed controller bug

--- routes/api.php
+++ routes/api.php
@@ -24,4 +24,4 @@ Route::post('/isnumberavailable', 'UserController@isNumberAvailableInDatabase');
 Route::post('/updateuserprofile', 'UserController@updateUserProfile');
 Route::post('/searchpermanentjob', 'PermanentjobController@searchPermanentJob');
 Route::get('/getcollegelist', 'PermanentjobController@getCollegeList');
-Route::post('/changestatus', 'PermanentjobController@changeAvaibilityStatus');
+Route::post('/changestatus', 'UserController@changeAvaibilityStatus');
